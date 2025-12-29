f7ce8a7360d68ae3493523ded80246215bfd8d20
artgolwebdev <artyombb@gmail.com>
2025-12-29 21:41:13 +0200

    Configure for GitHub Pages: add base path and build to docs folder

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react-swc';
 import path from 'path';
 
 export default defineConfig({
+  base: '/dancer/',
   plugins: [react()],
   resolve: {
     extensions: ['.js', '.jsx', '.ts', '.tsx', '.json'],
@@ -51,7 +52,7 @@ export default defineConfig({
   },
   build: {
     target: 'esnext',
-    outDir: 'build',
+    outDir: 'docs',
   },
   server: {
     port: 3000,
